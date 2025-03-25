f1e4a8ccc1f1ccd22b080c1da5e3c60168322f1f
Tuuli Perttula <perttula@kat-laskenta.fmi.fi>
2025-03-25 11:31:22 +0000

    Added separate function for plotting Ukraine data

--- plot_tropomi.py
+++ plot_tropomi.py
@@ -142,6 +142,65 @@ def plot_data(figname, latitudes, longitudes, obs_data, description, unit, conf,
     fig.savefig(figname, bbox_inches = 'tight') #, dpi = 300)
 
 
+def plot_data_ukraine(figname, latitudes, longitudes, obs_data, description, unit, conf, timeperiod, datetime_start, datetime_stop, logos, fmi_logo):
+    """ Plot satellite data and logos
+
+    Keyword arguments:
+    figname -- filename for saving plot
+    latitudes -- observation latitude data
+    longitudes -- observation longitude data
+    obs_data -- data values
+    description -- data description
+    unit -- data unit
+    conf -- config dictionary
+    timeperiod -- length of merged data to plot, options: day|month
+    datetime_start -- first timestamp of data
+    datetime_stop -- last timestamp of data
+    logos -- logos image to be added in the picture
+ 
+    """
+
+    # Read config plot parameter
+    vmin = conf["plot"][timeperiod]["vmin"]
+    vmax = conf["plot"][timeperiod]["vmax"]
+    colormap = conf["plot"][timeperiod]["colormap"]
+    
+    print("np.nanmin(obs_data)", np.nanmin(obs_data))
+    print("np.nanmax(obs_data)", np.nanmax(obs_data))
+    
+    # Create plot
+    logger.debug('Plotting image')
+    fig, axs = plt.subplots(figsize=(20,10))
+
+    # Plot map
+    ax = plt.axes(projection = ccrs.PlateCarree())
+    ax.set_extent([10, 55, 20, 55], ccrs.PlateCarree())
+    img = plt.pcolormesh(longitudes, latitudes, obs_data, vmin = vmin, vmax = vmax, cmap = colormap, transform = ccrs.PlateCarree())
+    ax.coastlines()
+    ax.gridlines()
+    ax.set_title(f"L3 merged product of {description} \n First timestamp: {datetime_start}   Last timestamp: {datetime_stop}", fontsize=16)
+
+    # Add colorbar
+    cbar = fig.colorbar(img, fraction=0.046, pad=0.02, shrink=0.91, aspect=20*0.91)
+    cbar.set_label(f'{description} [{unit}]',fontsize=15)
+    cbar.ax.tick_params(labelsize=14)
+
+    # Add logos
+    newax = fig.add_axes([0.13, 0.07, 0.5, 0.05], anchor='SW')
+    newax.imshow(logos)
+    newax2 = fig.add_axes([0.13, 0.87, 0.5, 0.05], anchor='SW')
+    newax2.imshow(fmi_logo)
+
+    # Remove extra axis
+    newax.axis('off')
+    newax2.axis('off')
+    axs.axis('off')
+
+    # Save figure to file
+    logger.debug(f'Save image to file {figname}')
+    fig.savefig(figname, bbox_inches = 'tight') #, dpi = 300)
+
+
 def main():
 
     # Read config file into dictionary
@@ -163,7 +222,10 @@ def main():
 
     # Plot data
     figname = f'{conf["output"][timeperiod]["path"]}/{conf["output"][timeperiod]["filename"].format(date = options.date)}'
-    plot_data(figname, latitudes, longitudes, obs_data, description, unit, conf, timeperiod, datetime_start, datetime_stop, logos, fmi_logo)
+    if not "ukraine" in options.var:
+        plot_data(figname, latitudes, longitudes, obs_data, description, unit, conf, timeperiod, datetime_start, datetime_stop, logos, fmi_logo)
+    else:
+        plot_data_ukraine(figname, latitudes, longitudes, obs_data, description, unit, conf, timeperiod, datetime_start, datetime_stop, logos, fmi_logo)
     
 
 if __name__ == '__main__':
